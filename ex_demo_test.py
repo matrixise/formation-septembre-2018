65a26638da5b1d9207f8044a4fe8d3994e32b4b9
Stéphane Wirtel <stephane@wirtel.be>
2018-09-18 12:13:41 +0200

    Add the tests for all the cases

--- ex_demo_test.py
+++ ex_demo_test.py
@@ -69,25 +69,45 @@ class ProductTestCase(unittest.TestCase):
 
 
 class InvoiceTestCase(unittest.TestCase):
-    def test_invoice(self):
+    def test_invoice_check_name(self):
         product = create_product('iPhone', 1000.0)
         invoice_line = create_invoice_line(product, 2.0)
         invoice = create_invoice('INV-2018/0001', 'Manfred', [invoice_line], vat=1.20)
 
         self.assertTrue(invoice['name'].startswith('INV-2018/'))
 
+    def test_invoice_check_number_of_lines(self):
+        product = create_product('iPhone', 1000.0)
+        invoice_line = create_invoice_line(product, 2.0)
+        invoice = create_invoice('INV-2018/0001', 'Manfred', [invoice_line], vat=1.20)
+
         self.assertEqual(len(invoice['invoice_lines']), 1)
 
-        # amount = sum(map(operator.attrgetter('amount'), invoice['invoice_lines']))
-        # self.assertEqual(invoice['amount'], amount)
+    def test_invoice_check_amount_functional(self):
+        product = create_product('iPhone', 1000.0)
+        invoice_line = create_invoice_line(product, 2.0)
+        invoice = create_invoice('INV-2018/0001', 'Manfred', [invoice_line], vat=1.20)
+
+        amount = sum(map(operator.attrgetter('amount'), invoice['invoice_lines']))
+        self.assertEqual(invoice['amount'], amount)
+
+    def test_invoice_check_amount_with_list_comprehension(self):
+        product = create_product('iPhone', 1000.0)
+        invoice_line = create_invoice_line(product, 2.0)
+        invoice = create_invoice('INV-2018/0001', 'Manfred', [invoice_line], vat=1.20)
 
         amount = sum(line.amount for line in invoice['invoice_lines'])
         self.assertEqual(invoice['amount'], amount)
 
-        # amount = 0
-        # for line in invoice['invoice_lines']:
-        #     amount = amount + line.amount
-        # self.assertEqual(invoice['amount'], amount)
+    def test_invoice_check_amount_with_for_loop(self):
+        product = create_product('iPhone', 1000.0)
+        invoice_line = create_invoice_line(product, 2.0)
+        invoice = create_invoice('INV-2018/0001', 'Manfred', [invoice_line], vat=1.20)
+
+        amount = 0
+        for line in invoice['invoice_lines']:
+            amount = amount + line.amount
+        self.assertEqual(invoice['amount'], amount)
 
 
 if __name__ == '__main__':
