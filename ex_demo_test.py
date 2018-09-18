ef87818576a16aca01defd1577ed9eeec985746e
Stéphane Wirtel <stephane@wirtel.be>
2018-09-18 12:10:33 +0200

    Split the invoice test in the InvoiceTestCase

--- ex_demo_test.py
+++ ex_demo_test.py
@@ -67,12 +67,15 @@ class ProductTestCase(unittest.TestCase):
         amount = product.price * 2.0
         self.assertEqual(amount, invoice_line.amount)
 
+
+class InvoiceTestCase(unittest.TestCase):
     def test_invoice(self):
         product = create_product('iPhone', 1000.0)
         invoice_line = create_invoice_line(product, 2.0)
         invoice = create_invoice('INV-2018/0001', 'Manfred', [invoice_line], vat=1.20)
 
         self.assertTrue(invoice['name'].startswith('INV-2018/'))
+
         self.assertEqual(len(invoice['invoice_lines']), 1)
 
         # amount = sum(map(operator.attrgetter('amount'), invoice['invoice_lines']))
@@ -85,6 +88,7 @@ class ProductTestCase(unittest.TestCase):
         # for line in invoice['invoice_lines']:
         #     amount = amount + line.amount
         # self.assertEqual(invoice['amount'], amount)
+
 
 if __name__ == '__main__':
     unittest.main()
